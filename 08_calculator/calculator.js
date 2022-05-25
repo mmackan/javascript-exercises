19802c57608c335447928c7ac2d9292914f0add4
mmackan <69105418+mmackan@users.noreply.github.com>
2022-05-25 08:20:24 +0200

    Add calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  return numbers.reduce((tot, num) => {return tot + num;}, 0);	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return add(...numbers);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((tot, num) => {return tot * num;}, 1);
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+  let arr = [];
+  for(let i = 0; i < pow; i++) { arr.push(base); }
+
+  return multiply(arr);	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) { return 1; }
+  return num * (factorial(num-1));  
 };
 
 // Do not edit below this line
